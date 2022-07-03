48ace19b802a838d09fb5f16352e1642c40e499b
Matt Zhang <set.stun@gmail.com>
2022-07-03 02:38:22 -0400

    Website republish

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "ae44cfba99f99e1e9abe262f52af9818",
-"index.html": "3ddf2ed850704c811e07029b6a9f7adf",
-"/": "3ddf2ed850704c811e07029b6a9f7adf",
+"index.html": "b89d92a35b823de80a4ff95b9af30153",
+"/": "b89d92a35b823de80a4ff95b9af30153",
 "styles.css": "8bdeefb87ca882d8f48f712db15f3adf",
-"main.dart.js": "411dd4099e10e1f5645451b6e23d337f",
+"main.dart.js": "f043cc681c1f83d63752aac59f7d704a",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "faf5d90a9f58c162fa45f59f5acd8437",
 "icons/Icon-192.png": "4675a24bab7248b88be6532c906f8e55",
